84a76c70c830349dddc8cbf40ac74b21e40f61ef
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-16 22:22:00 +0200

    Add files via upload

---
+++ Листинг 1/Листинг 1.1/Program.cs
@@ -0,0 +1,112 @@
+﻿using static System.Console;
+//creating an abstract class
+abstract class Base
+{
+    //creating a protected variable field
+    protected int num;
+    //creating a constructor
+    public Base(int n)
+    {
+        //using the set method
+        set(n);
+    }
+    //creating 3 abstract methods
+    public abstract void show();
+    public abstract void set(int n);
+    public abstract int get();
+}
+//creating a class that inherits from Base
+class Alpha : Base
+{
+    //creating a protected variable field
+    protected int val;
+    //creating a constructor with a reference to the base class constructor
+    public Alpha(int n) : base(n)
+    {
+        //using the method show
+        show();
+    }
+    //overloading the void method
+    public override void show()
+    {
+        //displaying values from Alpha
+        WriteLine("Alpha: {0},{1} и {2}", num, val, get());
+    }
+    //overloading the set method
+    public override void set(int n)
+    {
+        //assigning the variables value
+        num = n;
+        //assigning the variables value 
+        val = n % 10;
+    }
+    //overloading a get method
+    public override int get()
+    {
+        //returns the filed divided by 10
+        return num / 10;
+    }
+}
+//creating a class that inherits from Alpha
+class Bravo : Alpha
+{
+    //creating a protected field variable
+    protected int val;
+    //creating a constructor with a reference to the base class
+    public Bravo(int n) : base(n)
+    {
+        //using the method show
+        show();
+    }
+    //overloading the method show
+    public override void show()
+    {
+        //displaying value of Bravo
+        WriteLine("Bravo: {0},{1} и {2}", num, val, get());
+    }
+    //overloading a set method
+    public override void set(int n)
+    {
+        //assigning values to the variables
+        num = n;
+        val = n % 100;
+    }
+    //overloading a get method
+    public override int get()
+    {
+        //returns the filed divided by 10
+        return num / 100;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating a class variable
+        Base obj;
+        //creating 2 class objects
+        Alpha A = new Alpha(123);
+        Bravo B = new Bravo(321);
+
+        //assigning the class variables value to the object A
+        obj = A;
+        WriteLine("След изпълнение на командата obj = A");
+
+        //using the method set form obj
+        obj.set(456);
+        //using the method show form obj
+        obj.show();
+
+        //assigning the class variables value to the object B
+        obj = B;
+        WriteLine("След изпълнение на командата obj = B");
+
+        //using the method set form obj
+        obj.set(654);
+        //using the method show form obj
+        obj.show();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
---
+++ Листинг 1/Листинг 1.2/Program.cs
@@ -0,0 +1,132 @@
+﻿using static System.Console;
+//creating an abstract class
+abstract class Base{
+    //creating an abstract property
+    public abstract string text{
+        //defining a get and a set accessor
+        get;
+        set;
+    }
+    //creating an abstract indexer
+    public abstract char this[int k]{
+        //defining a get accessor
+        get;
+    }
+    //creating an abstract property
+    public abstract int length{
+        //defining a get accessor
+        get;
+    }
+}
+//creating a class that inherits from Base
+class Alpha : Base{
+    //creating a private char array field
+    private char[] symbs;
+    //creating a constructor with a reference to the base class
+    public Alpha(string t) : base(){
+        //using the property to assign a value
+        text = t;
+    }
+    //overloading a property
+    public override string text{
+        //defining a get accessor
+        get{
+            //returns a new string of the elements of the array
+            return new string(symbs);
+        }
+        //defining a set accessor
+        set{ 
+            //assigning the the value of the elements of the array
+            symbs = value.ToCharArray();
+        }
+    }
+    //overloading a property
+    public override int length{
+        //defining a get accessor
+        get{
+            //returns the length of the array
+            return symbs.Length;
+        }
+    }
+    //overloading an indexer
+    public override char this[int k]{
+        //defining a get accessor
+        get{
+            //returns the index of the array
+            return symbs[k];
+        }
+    }
+}
+//creating a class that inherits from Base
+class Bravo : Base{
+    //creating a variable field
+    private string txt;
+    //creating a constructor with a reference to the base class
+    public Bravo(string t) : base(){
+        //assigning the fields value
+        txt = t;
+    }
+    //overloading a property
+    public override string text{
+        //defining a get accessor
+        get{
+            //returns the field
+            return txt;
+        }
+        //defining a set accessor
+        set{ 
+            //assigns the fields value
+            txt = value;
+        }
+    }
+    //overloading a property
+    public override int length{
+        //defining a get accessor
+        get{
+            //returns the length of the array
+            return txt.Length;
+        }
+    }
+    //overloading an indexer
+    public override char this[int k]{
+        //defining a get accessor
+        get{
+            //returns the index of the array
+            return txt[k];
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating a new class object
+        Base obj = new Alpha("Alpha");
+        //displaying the field using a property
+        WriteLine(obj.text);
+
+        //assigning the value of the field via  a property
+        obj.text = "Base";
+        //displaying each of the elements of the field
+        for(int i = 0; i < obj.length ;i++){
+            Write("|"+obj[i]);
+        }
+        WriteLine("|");
+
+        //assigning the class variables value
+        obj = new Bravo("Bravo");
+        //displaying each of the elements of the field
+        for(int i = 0; i < obj.length ;i++){
+            Write("|"+obj[i]);
+        }
+        WriteLine("|");
+
+        //assigning the value of the field via  a property
+        obj.text = "Base";
+        //assigning the value of the field via  a property
+        WriteLine(obj.text);
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
