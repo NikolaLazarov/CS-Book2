2b15215c4b23ad7ef062f92fc78d04ec137498ad
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-20 22:10:19 +0200

    Add files via upload

---
+++ Листинг 1/Листинг 1.7/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+//creating an interface
+interface First
+{
+    //creating a method
+    void show();
+}
+//creating an interface
+interface Second
+{
+    //creating a method
+    void show();
+}
+//creating a class that inherits from two interfaces
+class myClass : First, Second
+{
+    //creating a method
+    public void show()
+    {
+        //displaying text
+        WriteLine("Object of the class myClass.");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating a class object
+        myClass obj = new myClass();
+        //creating a interface variable and assigning its value to obj
+        First A = obj;
+        //creating a interface variable and assigning it value to obj
+        Second B = obj;
+
+        //using the method show from in 3 different ways 
+        obj.show();
+        A.show();
+        B.show();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
