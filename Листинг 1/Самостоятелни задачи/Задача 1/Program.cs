deb446c8121d1f431f3e2016333ad6cd43055d34
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-22 21:34:26 +0200

    Add files via upload

---
+++ Листинг 1/Самостоятелни задачи/Задача 1/Program.cs
@@ -0,0 +1,70 @@
+﻿using static System.Console;
+abstract class Base
+{
+    protected int[] arrNums;
+    public Base(int n)
+    {
+        arrNums = new int[n];
+    }
+    public abstract int length
+    {
+        get;
+    }
+    public abstract int this[int n]
+    {
+        get;
+        set;
+    }
+    public abstract void show();
+}
+class myClass : Base
+{
+    public myClass(int n) : base(n)
+    {
+        arrNums = new int[n];
+    }
+    public override int length
+    {
+        get
+        {
+            return arrNums.Length;
+        }
+    }
+    public override int this[int n]
+    {
+        get
+        {
+            return arrNums[n];
+        }
+        set
+        {
+            arrNums[n] = value;
+        }
+    }
+    public override void show()
+    {
+        for (int i = 0; i < arrNums.Length; i++)
+        {
+            Write(arrNums[i] + " ");
+        }
+        WriteLine();
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass A = new myClass(5);
+
+        WriteLine(A.length);
+        A[0] = 6;
+        A[1] = 9;
+        A[2] = 4;
+        A[3] = 2;
+        A[4] = 0;
+        A.show();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
---
+++ Листинг 1/Самостоятелни задачи/Задача 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+abstract class Base{
+    protected string text;
+    public abstract int length{
+        get;
+    }
+    public abstract char this[int k]{
+        get;
+    }
+    public abstract void ass(string txt);
+    public abstract void show();
+}
+class myClass : Base{
+    public myClass(string txt) : base(){
+        text = txt;
+    }
+    public override int length{
+        get{
+            return text.Length;
+        }
+    }
+    public override char this[int k]{
+        get{
+            return text[k];
+        }
+    }
+    public override void ass(string txt){ 
+        text = txt;
+    }
+    public override void show(){
+        WriteLine("\""+text+"\"");
+    }
+}
+public class Program
+{
+    public static  void Main(string[] args)
+    {
+        myClass A = new myClass("Yokenshnine");
+        
+        A.show();
+        WriteLine(A.length);
+        WriteLine(A[6]);
+        WriteLine(A[9]);
+        
+        A.ass("Frank Herbert");
+        
+        A.show();
+        WriteLine(A.length);
+        WriteLine(A[3]);
+        WriteLine(A[8]);
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
