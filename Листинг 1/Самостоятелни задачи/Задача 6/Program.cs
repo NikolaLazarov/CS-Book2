2365765f0f9f4d7ee9504daee194afa6919c5b25
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-23 20:57:37 +0200

    Add files via upload

---
+++ Листинг 1/Самостоятелни задачи/Задача 6/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+interface myItf1
+{
+    int this[char s]
+    {
+        get;
+    }
+}
+interface myItf2
+{
+    char this[int k]
+    {
+        get;
+    }
+}
+class myClass : myItf1, myItf2
+{
+    public int this[char s]
+    {
+        get
+        {
+            return (int)s;
+        }
+    }
+    public char this[int k]
+    {
+        get
+        {
+            return (char)k;
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass A = new myClass();
+
+        WriteLine(A['☻']);
+        WriteLine(A[9829]);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
