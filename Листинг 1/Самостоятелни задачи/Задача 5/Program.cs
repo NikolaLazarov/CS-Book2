bbd3f77f0d3d538e31a32a72be0008e4869c5349
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-23 17:42:10 +0200

    Add files via upload

---
+++ Листинг 1/Самостоятелни задачи/Задача 5/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+interface myItf1
+{
+    char meth(int n);
+}
+interface myItf2
+{
+    int meth(char s);
+}
+class myClass : myItf1, myItf2
+{
+    public char meth(int n)
+    {
+        return (char)n;
+    }
+    public int meth(char s)
+    {
+        return (int)s;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass obj = new myClass();
+        WriteLine(obj.meth(69));
+        WriteLine(obj.meth('E'));
+
+        myItf1 A = obj;
+        WriteLine(A.meth(69));
+
+        myItf2 B = obj;
+        WriteLine(B.meth('E'));
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
