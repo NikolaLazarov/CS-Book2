d7159da9156946adec9ee76f7b18b376047e4812
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-03-23 22:42:12 +0200

    Add files via upload

---
+++ Листинг 1/Самостоятелни задачи/Задача 10/Program.cs
@@ -0,0 +1,100 @@
+﻿using static System.Console;
+abstract class Base
+{
+    public abstract int number
+    {
+        get;
+        set;
+    }
+    public abstract char this[int k]
+    {
+        get;
+        set;
+    }
+    public abstract void show();
+}
+interface myItf1
+{
+    public abstract int number
+    {
+        get;
+        set;
+    }
+    public abstract char this[int k]
+    {
+        get;
+        set;
+    }
+    public abstract void show();
+}
+interface myItf2
+{
+    public abstract int number
+    {
+        get;
+        set;
+    }
+    public abstract char this[int k]
+    {
+        get;
+        set;
+    }
+    public abstract void show();
+}
+class myClass : Base, myItf1, myItf2
+{
+    int a;
+    string t = "";
+    public override int number
+    {
+        get
+        {
+            return a;
+        }
+        set
+        {
+            a = value;
+        }
+    }
+    int myItf1.number
+    {
+        get
+        {
+            return a;
+        }
+        set
+        {
+            a = value;
+        }
+    }
+    int myItf2.number
+    {
+        get
+        {
+            return a;
+        }
+        set
+        {
+            a = value;
+        }
+    }
+    public override char this[int k]
+    {
+        get
+        {
+            return t[k];
+        }
+        set
+        {
+            t[k] = value;
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
