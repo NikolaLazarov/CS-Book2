cfefe930768828e434e8231e4cca18d1d9f1fd11
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-08 23:41:13 +0300

    Add files via upload

---
+++ Листинг 2/Листинг 2.14/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+//creating a class
+class myClass
+{
+    //creating a private string field
+    private string text;
+    //creating a constructor that assigns the value of the field
+    public myClass(string t)
+    {
+        text = t;
+    }
+    //creating an indexer with an lambda accessor
+    public char this[int k] => text[k];
+    //creating a property with a lambda accessor
+    public int length => text.Length;
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating a class object
+        myClass obj = new myClass("Alpha");
+        //creating a loop that repeats obj.length times
+        for (int i = 0; i < obj.length; i++)
+        {
+            //displaying the indexer
+            Write("|" + obj[i]);
+        }
+        WriteLine("|");
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
