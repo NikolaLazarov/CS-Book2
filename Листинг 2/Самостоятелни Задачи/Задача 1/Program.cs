475f2e86770b20893c9d82f715bac4e5b582f0dc
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-14 22:39:06 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+delegate int myDelegate(char a, string b);
+public class Program
+{
+    public static int meth1(char a, string b)
+    {
+        int n = 0;
+        for (int i = 0; i < b.Length; i++)
+        {
+            if (b[i] == a)
+            {
+                n++;
+            }
+        }
+        return n;
+    }
+    public static int meth2(char a, string b)
+    {
+        return b.IndexOf(a);
+    }
+    public static void Main(string[] args)
+    {
+        myDelegate md = meth1;
+        WriteLine(md('e', "The quick brown fox jumped over the lazy dog"));
+
+        md = meth2;
+        WriteLine(md('e', "The quick brown fox jumped over the lazy dog"));
+`
+        WriteLine("Press any key to close...");
+    }
+}
---
+++ Листинг 2/Самостоятелни Задачи/Задача 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+delegate void myDelegate(char s);
+class myClass{
+    public char symbol;
+    public void set(char s){
+        symbol = s;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass[] objs = new myClass[3];
+        myDelegate[] delegates = new myDelegate[3];
+        for(int i = 0; i < objs.Length; i++){
+            Random rnd = new Random();
+            delegates[i] = objs[i].set;
+            delegates[i]((char)rnd.Next(65,91));
+        }
+        for(int i = 0; i < objs.Length; i++){
+            WriteLine(delegates[i]);
+        }
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
