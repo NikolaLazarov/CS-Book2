a3b0ca31f84e94ad096af182cdec956654aaecdf
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-19 18:58:47 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+delegate int myDelegate();
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        int a = 0, b = 1;
+        myDelegate md = () =>
+        {
+            b += a;
+            a = b - a;
+            return b;
+        };
+
+        WriteLine("Fibonacci number: 1 = 1");
+        for (int i = 2; i <= 10; i++)
+        {
+            Write("Fibonacci number: " + i + " = " + md() + "\n");
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
