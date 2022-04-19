f183a6eeb240f6896e9d72aac996105ac36cb379
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-19 18:38:26 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+delegate int myDelegate(int n);
+class myClass
+{
+    public myDelegate md;
+    public myDelegate this[int n]
+    {
+        get
+        {
+            return md = i =>
+            {
+                double d = Math.Pow(n, i);
+                return (int)d;
+            };
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass obj = new myClass();
+        int n = obj[2](3);
+        WriteLine(n);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
