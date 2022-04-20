504eaa97db8ef065068c95b692bd4f22d794592c
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-20 22:51:42 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 6/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+delegate string myDelegate();
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int day = -1;
+        string[] weekDays = new string[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+        myDelegate md = () =>
+        {
+            day++;
+            if (day > 6) day = 0;
+            return weekDays[day];
+        };
+
+        for (int i = 0; i < 9; i++)
+        {
+            Write(md() + "\n");
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
