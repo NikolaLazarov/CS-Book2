6a4753665c3c91f2ff8b175a8615f23f543a9e79
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-24 19:53:24 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 8/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+delegate int myDelegate(int n);
+public class Program
+{
+    static  int meth(myDelegate md1, myDelegate md2){
+       return md1(md2);
+    }
+    static int meth1(int n){
+        return n*n;
+    }
+    static int meth2(int n){
+        return n*n*n;
+    }
+    public static void Main(string[] args)
+    {
+        myDelegate md = meth(meth1,meth2);
+        WriteLine(md);
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
