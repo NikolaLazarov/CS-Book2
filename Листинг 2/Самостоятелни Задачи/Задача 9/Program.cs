056a3a513660994de9ca4f8d7e099cfb0db3a5f0
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-24 22:21:30 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 9/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+public class Program
+{
+    static string WalterHWhite(params double[] args)
+    {
+        double x1 = (-args[1] + Math.Sqrt(args[1] * args[1] - 4 * args[0] * args[2])) / 2;
+        double x2 = (args[1] + Math.Sqrt(args[1] * args[1] - 4 * args[0] * args[2])) / 2;
+        string qF = args[0] + "x² +" + args[1] + "x + " + args[2] + " = x1 = " + x1 + "/x2 = " + x2;
+        return qF;
+    }
+    public static void Main(string[] args)
+    {
+        WriteLine(WalterHWhite(1, -11, 28));
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
