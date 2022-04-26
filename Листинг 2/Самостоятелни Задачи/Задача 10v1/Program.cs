eede9476a611fe65d6c76c9a4477a1309840bac3
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-04-26 21:55:30 +0300

    Add files via upload

---
+++ Листинг 2/Самостоятелни Задачи/Задача 10v1/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+delegate void myDelegate(string txt);
+class myClass
+{
+    public event myDelegate myEvent;
+    public void evoke(string txt)
+    {
+        if (myEvent != null)
+        {
+            myEvent(txt);
+        }
+    }
+}
+class Alpha
+{
+    private string name;
+    public Alpha(string txt)
+    {
+        name = txt;
+    }
+    public void show(string txt)
+    {
+        WriteLine("Object " + name + ":");
+        WriteLine(txt);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass obj = new myClass();
+        Alpha A = new Alpha("A");
+        Alpha B = new Alpha("B");
+
+        obj.myEvent += A.show;
+        obj.evoke("Fowl burger");
+        WriteLine();
+        
+        obj.myEvent += B.show;
+        obj.evoke("Double filet burger");
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
---
+++ Листинг 2/Самостоятелни Задачи/Задача 10v2/Program.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+delegate void myDelegate(string txt);
+class myClass
+{
+    public myDelegate myevent;
+    public event myDelegate myEvent
+    {
+        add
+        {
+            myevent += value;
+        }
+        remove
+        {
+            myevent -= value;
+        }
+    }
+    public void evoke(string msg)
+    {
+        if (myevent != null)
+        {
+            myevent(msg);
+        }
+    }
+}
+class Alpha
+{
+    private string name;
+    public Alpha(string txt)
+    {
+        name = txt;
+    }
+    public void show(string msg)
+    {
+        WriteLine("Object " + name + ":");
+        WriteLine(msg);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass obj = new myClass();
+        Alpha A = new Alpha("A");
+        Alpha B = new Alpha("B");
+
+        obj.myEvent += A.show;
+        obj.evoke("Cooler");
+        WriteLine();
+
+        obj.myEvent += B.show;
+        obj.evoke("Radiator");
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
