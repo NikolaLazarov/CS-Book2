194a6a2774c6f2605b8e1dd382cd65a9f8b6faf8
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-08 22:50:01 +0300

    Add files via upload

---
+++ Листинг 3/Самостоятелни Задачи/Задача 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+enum weekdays { Mon, Tue, Wed, Thu, Fri, Sat, Sun };
+public class Program
+{
+    static string day(int n)
+    {
+        string[] wdays = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+        return wdays[n];
+    }
+    static void day(int a, int b)
+    {
+        WriteLine(b - a - 1);
+    }
+    public static void Main(string[] args)
+    {
+        weekdays wday = weekdays.Mon;
+        for (int i = 0; i < 7; i++)
+        {
+            WriteLine(day(i));
+        }
+        WriteLine();
+        day(1, 5);
+        day(6, 7);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
