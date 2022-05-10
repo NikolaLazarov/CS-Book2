7f22eb64a4fc6a65e2c83868b1022a55ef5f0b57
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-10 20:26:20 +0300

    Update Program.cs

--- Листинг 3/Самостоятелни Задачи/Задача 2/Program.cs
+++ Листинг 3/Самостоятелни Задачи/Задача 2/Program.cs
@@ -1,8 +1,46 @@
-﻿using static System.Console;
+using static System.Console;
+struct NIGHT
+{
+    private int numF;
+    private char charF;
+    private string stringF;
+    public NIGHT(int a, char b, string c)
+    {
+        numF = a;
+        charF = b;
+        stringF = c;
+    }
+    public NIGHT(int a, string c)
+    {
+        numF = a;
+        charF = '☻';
+        stringF = c;
+    }
+    public NIGHT(int a)
+    {
+        numF = a;
+        charF = '☻';
+        stringF = "♣";
+    }
+    public void show()
+    {
+        WriteLine(numF);
+        WriteLine(charF);
+        WriteLine(stringF);
+    }
+}
 public class Program
 {
     public static void Main(string[] args)
     {
+        NIGHT A = new NIGHT(7, 'R', "Knight");
+        NIGHT B = new NIGHT(3, "Deltoid");
+        NIGHT C = new NIGHT(5);
+
+        A.show();
+        B.show();
+        C.show();
+
         WriteLine("Press any key to close...");
         ReadKey(true);
     }
