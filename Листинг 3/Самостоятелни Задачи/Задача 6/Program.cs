7dbdb7412e9dee15eb5b54920f9ccb63f1e2ce1d
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-13 19:18:07 +0300

    Update Program.cs

--- Листинг 3/Самостоятелни Задачи/Задача 6/Program.cs
+++ Листинг 3/Самостоятелни Задачи/Задача 6/Program.cs
@@ -1,9 +1,64 @@
-﻿using static System.Console;
-public class Program
+using static System.Console;
+struct _struct
 {
-    public static void Main(string[] args)
+    public int a;
+    public int b;
+    public _struct(int a, int b)
     {
-        WriteLine("Press any key to close...");
-        ReadKey(true);
+        this.a = a;
+        this.b = b;
+    }
+    public static _struct operator +(_struct a, _struct b)
+    {
+        int f = a.a + b.a;
+        int c = a.b + b.b;
+        return new _struct(f, c);
+    }
+    public static _struct operator -(_struct a, _struct b)
+    {
+        int f = a.a - b.a;
+        int c = a.b - b.b;
+        return new _struct(f, c);
+    }
+    public static _struct operator *(_struct a, _struct b)
+    {
+        int f = a.a * b.a;
+        int c = a.b * b.b;
+        return new _struct(f, c);
+    }
+    public static int operator ^(_struct a, _struct b)
+    {
+        int[] arrNums = { a.a, a.b, b.a, b.b };
+        int y = arrNums.Max() * 100 + arrNums.Min();
+        return y;
+    }
+    public override string ToString()
+    {
+        string str = "";
+        str += "Field a: " + a;
+        str += "\nField b: " + b;
+        str += "\n-------------";
+        return str;
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            _struct A = new _struct(2, 3);
+            _struct B = new _struct(3, 4);
+            _struct C = A + B;
+            _struct E = A - B;
+            _struct G = A * B;
+            WriteLine(A);
+            WriteLine(B);
+            WriteLine(C);
+            WriteLine(E);
+            WriteLine(G);
+            WriteLine(A ^ B);
+            WriteLine(E ^ G);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
     }
 }
