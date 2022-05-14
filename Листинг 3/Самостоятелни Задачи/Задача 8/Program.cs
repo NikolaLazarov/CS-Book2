e4a558add7d0e77f6aa6f73a35aae978a919b0bc
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-15 00:25:47 +0300

    Add files via upload

---
+++ Листинг 3/Самостоятелни Задачи/Задача 8/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+struct A
+{
+    public string s;
+    public A(string s)
+    {
+        this.s = s;
+    }
+    public B ass(int n)
+    {
+        return new B(s[n]);
+    }
+}
+struct B
+{
+    public char s;
+    public B(char s)
+    {
+        this.s = s;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        A A = new A("Water");
+        B B = A.ass(0);
+        WriteLine(B.s);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
