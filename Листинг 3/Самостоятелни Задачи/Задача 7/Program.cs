4ffb2bb057db3265ffe4a2acbf3bea874a44d418
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-15 00:11:50 +0300

    Update Program.cs

--- Листинг 3/Самостоятелни Задачи/Задача 7/Program.cs
+++ Листинг 3/Самостоятелни Задачи/Задача 7/Program.cs
@@ -1,8 +1,50 @@
-﻿using static System.Console;
+using static System.Console;
+struct _struct
+{
+    private char s;
+    public _struct(char t)
+    {
+        s = t;
+    }
+    public _struct[] ass(string t)
+    {
+        _struct[] arrStr = new _struct[t.Length];
+        for (int i = 0; i < arrStr.Length; i++)
+        {
+            arrStr[i] = new _struct(t[i]);
+        }
+        return arrStr;
+    }
+    public string asss(_struct[] s)
+    {
+        string str = "";
+        for (int i = 0; i < s.Length; i++)
+        {
+            str += s[i].s;
+        }
+        return str;
+    }
+    public override string ToString()
+    {
+        string str = "";
+        str += s;
+        return str;
+    }
+}
 public class Program
 {
     public static void Main(string[] args)
     {
+        _struct A = new _struct();
+        _struct[] B = A.ass("Moneky");
+        for (int i = 0; i < B.Length; i++)
+        {
+            Write(B[i] + " ");
+        }
+        WriteLine();
+        string s = A.asss(B);
+        WriteLine(s);
+
         WriteLine("Press any key to close...");
         ReadKey(true);
     }
