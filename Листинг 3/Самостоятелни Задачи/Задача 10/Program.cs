dd805691544622f34e30f9b79168e90eb58b826e
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-18 00:36:51 +0300

    Add files via upload

---
+++ Листинг 3/Самостоятелни Задачи/Задача 10/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+struct A{
+    int a;
+    public A(int a){
+        this.a = a;
+    }
+    public static B operator + (A C, A F){
+        return new B(C.a, F.a);
+    }
+}
+struct B{
+    int a, b;
+    public int[] arrFields = new int[2];
+    public B(int a, int b){
+        this.a = a;
+        this.b = b;
+        arrFields[0] = a;
+        arrFields[1] = b;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        A A = new A(7);
+        A B = new A(6);
+        B C = A + B;
+        for(int i = 0; i < C.arrFields.Length; i++){
+            Write(C.arrFields[i]);
+        }
+        WriteLine();
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
