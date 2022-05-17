613836b93f445f9c44d6250b28dbabbb9145e9d3
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-17 23:53:15 +0300

    Add files via upload

---
+++ Листинг 3/Самостоятелни Задачи/Задача 9/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
