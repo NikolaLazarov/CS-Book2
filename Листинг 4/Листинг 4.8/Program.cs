b2863eaa306e1994b8907a569db00d60b25e2eec
Nikola Lazarov <hazegamingbg@gmail.com>
2022-05-27 23:16:42 +0300

    listing4; 8/9
    Some often repent, yet never reform; they resemble a man traveling in a dangerous path, who frequently starts and stops, but never turns back.

---
+++ Листинг 4/Листинг 4.8/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        //creating 2 variables from type int
+        int A,B;
+        //creating an int type pointer
+        int* p;
+        //creating a double pointers
+        int** q;
+
+        //assigning the address of p to q;
+        q = &p;
+        //assigning the address of p to A
+        p = &A;
+
+        //assigning the value of q
+        **q = 123;
+        //displaying A
+        WriteLine(A);
+        //assigning the address of B to q;
+        *q = &B;
+
+        //assigning the value of q    
+        *p = 321;
+        //displaying B
+        WriteLine(B);
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
