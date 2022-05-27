91f6be1cf9d35d5ddf2cde71fb99d65bcdb551a8
Nikola Lazarov <hazegamingbg@gmail.com>
2022-05-27 22:53:26 +0300

    listing4: almost
    If you want your neighbor to know what the Christ spirit will do for him, let him see what it has done for you.

---
+++ Листинг 4/Листинг 4.7/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        //creating a variable from class String and assigning its value
+        String txt = "Programming in C#";
+        //displaying the variable
+        WriteLine(txt);
+
+        //creating a fixed function
+        fixed (char* p = txt)
+        {
+            /*creating a loop that displays the values 
+            of the variable and assigns new ones*/
+            for (int i = 0; p[i] != '\0'; i++)
+            {
+                Write("|" + p[i]);
+                p[i] = (char)('A' + i);
+            }
+            WriteLine("|");
+        }
+        //displaying the variable
+        WriteLine(txt);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
