d5c83daf56c11a362d36a841811d3eddbb0cb23a
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-05-30 22:22:38 +0300

    Add files via upload

---
+++ Листинг 4/Самостоятлени задачи/Задача 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        Random rnd1 = new Random();
+        Random rnd2 = new Random();
+        int a = rnd1.Next(1, 9), b = rnd2.Next(1, 9), c;
+        int* add = &c;
+        add[0] = a;
+        add[1] = a;
+        add[2] = b;
+        add[3] = b;
+        for (int i = 0; i < sizeof(int); i++)
+        {
+            Write((uint)add[i] + " ");
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
