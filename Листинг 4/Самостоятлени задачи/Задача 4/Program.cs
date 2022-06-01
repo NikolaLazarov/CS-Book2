7d22a588c35d472d3c616a5960dcac72ee048859
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-01 22:28:41 +0300

    idk man
    Jock, when ye hae naething else to do, ye may be aye sticking in a tree; it will be growing, Jock, when ye 're sleeping.

---
+++ Листинг 4/Самостоятлени задачи/Задача 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int a = rnd.Next(1, 9);
+        int* b = &a;
+        int f = (int)&b[0], s = (int)&b[1], t = (int)&b[2], fo = (int)&b[3];
+        b[0] = fo;
+        b[1] = f;
+        b[2] = s;
+        b[3] = t;
+        for (int i = 0; i < sizeof(int); i++)
+        {
+            Write((uint)b[i] + " ");
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
