2b2c18978c06c04f607268415ab4732de7f0be88
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-02 21:32:00 +0300

    listing4e: ALMOSTT
    You cannot help the poor man by destroying the rich.

---
+++ Листинг 4/Самостоятлени задачи/Задача 9/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        String txt = "The quick brown fox jumps over the lazy dog.";
+        fixed (char* t = txt)
+        {
+            for (int i = txt.Length - 1; i > -1; i--)
+            {
+                WriteLine((uint)&t[i]);
+            }
+        }
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
