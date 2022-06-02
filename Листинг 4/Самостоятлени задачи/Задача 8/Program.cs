4b50089dea30e0875de0ecfd645e8d37e7b8c991
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-02 19:28:30 +0300

    listing4e: almost
    Waiting for the winds of change to sweep the clouds away. Waiting for the rainbow's end to cast its gold your way ... You don't get something for nothing. You can't have freedom for free.

---
+++ Листинг 4/Самостоятлени задачи/Задача 8/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        double var1;
+        double* var2 = &var1;
+        byte*[] arr = new byte*[sizeof(double)];
+        for (int i = 0; i < sizeof(double); i += 2)
+        {
+            arr[i] = (byte*)&var2[i];
+            if (i == 6)
+            {
+                i = 1;
+            }
+        }
+        for (int i = 0; i < sizeof(double); i++)
+        {
+            Write((byte)&arr[i][i] + " ");
+        }
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
