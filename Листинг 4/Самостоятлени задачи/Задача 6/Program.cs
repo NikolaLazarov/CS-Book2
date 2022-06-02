8ec24a292bb54d852bd8591478b00e5d894c07ea
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-02 17:56:31 +0300

    listing4e: 12/20
    The mathematician is fascinated with the marvelous beauty of the forms he constructs, and in their beauty he finds everlasting truth.

---
+++ Листинг 4/Самостоятлени задачи/Задача 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using static System.Console;
+class _class
+{
+    public int num;
+}
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        _class A = new _class();
+        fixed (int* n = &A.num)
+        {
+            n[0] = 'A';
+            n[1] = 'A';
+            n[2] = 'B';
+            n[3] = 'B';
+            for (int i = 0; i < sizeof(int); i++)
+            {
+                Write((char)(uint)n[i] + " ");
+            }
+            WriteLine();
+        }
+
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
