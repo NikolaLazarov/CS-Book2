913499891f665fc4b4d3bb9be52d7cd4d3cbe325
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-02 18:51:31 +0300

    listing4e: cachow
    Every creature is better alive than dead, men and moose and pine trees, and he who understands it aright will rather preserve its life than destroy it.

---
+++ Листинг 4/Самостоятлени задачи/Задача 7/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        int[] num = new int[2];
+        fixed (int* a = &num[0])
+        {
+            int u = 1;
+            for (int i = 0; i < sizeof(int); i++)
+            {
+                a[i] = u;
+                u++;
+            }
+        }
+        fixed (int* a = &num[1])
+        {
+            int u = 1;
+            for (int i = 0; i < sizeof(int); i++)
+            {
+                a[i] = u;
+                u++;
+            }
+        }
+        for (int i = 0; i < num.Length; i++)
+        {
+            Write(num[i] + " ");
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
