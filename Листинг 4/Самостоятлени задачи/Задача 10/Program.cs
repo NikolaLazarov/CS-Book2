4af380b81d953adedb3bebc42821bbecd5d99913
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-02 21:49:03 +0300

    listing4e: DONE(......sortof)
    My success has allowed me to strike out with a higher class of women.

---
+++ Листинг 4/Самостоятлени задачи/Задача 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+public class Program
+{
+    unsafe static void Main(string[] args)
+    {
+        int k = 0;
+        int* n = &k;
+        char** s = (char**)&n;
+        *s[0] = 'A';
+        s[1] = s[0];
+        *s[2] = 'B';
+        s[3] = s[2];
+        for (int i = 0; i < sizeof(int); i++)
+        {
+            Write("Char and int pointer: " + (uint)&s[i] + " " + (uint)n[i] + "\n");
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
