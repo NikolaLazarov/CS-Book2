6a6b7e890c61f315c7ad062b5bbbf52a9b168596
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-07 19:41:11 +0300

    Listing 5: unique
    When a man has been guilty of any vice or folly, the best atonement he can make for it is to warn others not to fall into the like.

---
+++ Листинг 5/Листинг 5.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //defining a try block
+        try
+        {
+            //defining an inner try block
+            try
+            {
+                WriteLine("There will be an error now.");
+                //generating an ArithmeticException and defining its error message
+                throw new ArithmeticException("Some error.");
+            }
+            //defining a catch block for a ArithmeticException
+            catch (ArithmeticException e)
+            {
+                //displaying the error massage of the exception
+                WriteLine(e.Message);
+                //generating another ArithmeticException
+                throw;
+            }
+        }
+        //defining a catch block for a ArithmeticException
+        catch (ArithmeticException e)
+        {
+            //displaying the error massage of the exception
+            WriteLine("Once more:\n" + e.Message);
+        }
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
