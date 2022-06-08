57cbd70e1550acbd166e9bd8b4a6229f5c90ba5a
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-08 19:13:30 +0300

    Listing 5: chad Listing
    By working faithfully eight hours a day, you might eventually get to be a boss and work twelve hours a day.

---
+++ Листинг 5/Листинг 5.6/Program.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+//creating a class that inherits from the subclass ApplicationException of class Exception
+class _exception : ApplicationException
+{
+    //creating a private int variable
+    private int code;
+    //creating a constructor with 2 arguments(int, string) and the constructor of ApplicationException
+    public _exception(int n, string txt) : base(txt)
+    {
+        //assigning the value of the variable
+        code = n;
+    }
+    //overloading a ToString method that displays the error message and the code of the exception
+    public override string ToString()
+    {
+        string res = Message + "\nCode of error: " + code;
+        return res;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //defining a try block
+        try
+        {
+            //throwing an exception
+            throw new _exception(123, "Big mistake.");
+        }
+        //defining a catch blocks in case of an exception from Exception
+        catch (Exception e)
+        {
+            //displaying the exception
+            WriteLine(e);
+        }
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
