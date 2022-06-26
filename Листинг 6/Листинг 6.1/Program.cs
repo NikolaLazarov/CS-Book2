50197ced6d49ee4c5a9b3c52adef7a7dcbc37657
Nikola Lazarov <hazegamingbg@gmail.com>
2022-06-26 13:18:03 +0300

    Listing 6: The beginning
    ## Every form of addiction is bad, no matter whether the narcotic be alcohol or morphine or idealism.
    ### Carl Gustav Jung

---
+++ Листинг 6/Листинг 6.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+using System.Threading;
+public class Program
+{
+    //creating a static method
+    static void run(){
+        WriteLine("Secondary thread thread");
+        //Creating a loop that runs 6 times
+        for(int i = 0; i <= 5; i++){
+            //displaying i + 'A' converted to char
+            WriteLine("Secondary thread: {0}",(char)('A'+i));
+            //pausing for 1 second
+            Thread.Sleep(1000);
+            WriteLine("Secondary thread has ended");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        WriteLine("Starting main thread");
+        //creating a new object of class Thread with the method run as parameter
+        Thread md = new Thread(run);
+        //starting the secondary thread
+        md.Start();
+        //creating a loop that runs 5 times
+        for(int i = 0; i <= 5; i++){
+            //displaying i
+            WriteLine("Main thread:     "+i);
+            //pausing for 2 seconds
+            Thread.Sleep(2000);
+        }
+        WriteLine("Main thread has ended");
+        
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+} 
---
+++ Листинг 6/Листинг 6.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+using System.Threading;
+//creating a class
+class _class
+{
+    //creating 2 private int fields and 1 private string field
+    int count;
+    int time;
+    string text;
+    //creating a public method
+    public void show()
+    {
+        WriteLine(text + ": Begining.");
+        //creating a loop that runds count amount of times
+        for (int i = 0; i < count; i++)
+        {
+            WriteLine(text + "-> " + i);
+            //pausing for time amount of time
+            Thread.Sleep(time);
+        }
+        WriteLine(text + ": End.");
+    }
+    //creating a constructor that assignins the value of the fields
+    public _class(int c, int t, string txt)
+    {
+        count = c;
+        time = t;
+        text = txt;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        WriteLine("Main thread: Begining.");
+        //creating 3 class objects
+        _class obj1 = new _class(5, 2000, "Thread #1");
+        _class obj2 = new _class(6, 1200, "Thread #2");
+        _class obj3 = new _class(8, 1000, "Thread #3");
+
+        //creating 3 objects from Thread
+        Thread A = new Thread(obj1.show);
+        Thread B = new Thread(obj2.show);
+        Thread C = new Thread(obj3.show);
+
+        //starting all of the thread objects
+        A.Start();
+        B.Start();
+        C.Start();
+        WriteLine("Main thead: End.");
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
