3fcf583a31f7a2afd799dacdf5b88d0c65eeb695
Nikola Lazarov <hazegamingbg@gmail.com>
2022-07-03 18:37:41 +0300

    Listing 6 exercises: 3rd done

---
+++ Листинг 6/Самостоятелни задачи/Задача 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Array_Length = 10;
+        char[] Char_Array = new char[Array_Length];
+        Thread even = new Thread(() =>
+        {
+            char s = 'A';
+            for (int i = 0; i < Array_Length; i += 2)
+            {
+                Char_Array[i] = s;
+                s++;
+            }
+        });
+        Thread odd = new Thread(() =>
+        {
+            char s = 'А';
+            for (int i = 1; i < Char_Array.Length; i += 2)
+            {
+                Char_Array[i] = s;
+                s++;
+            }
+        });
+
+        even.Start();
+        odd.Start();
+        for (int i = 0; i < Char_Array.Length; i++)
+        {
+            Thread.Sleep(250);
+            Write(Char_Array[i]);
+        }
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
