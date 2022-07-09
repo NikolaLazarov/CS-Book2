7c89c564d422917c2841358bf5ec6d50b7983b8f
Nikola Lazarov <hazegamingbg@gmail.com>
2022-07-09 19:11:05 +0300

    Listing 6: DONE
    :anchor:

---
+++ Листинг 6/Самостоятелни задачи/Задача 10/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double Control_value = 2.0/3.0;
+        double sum = 1;
+        WriteLine("Control value: {0}",Control_value);
+        Thread PI = new Thread(()=>{
+            double n = 2;
+            while(true){
+                sum *= (Math.Pow(3,n)-1)/(Math.Pow(3,n)+1);
+                n++;
+                Thread.Sleep(10);                
+            }
+        });
+        PI.Start();
+        Thread.Sleep(100);
+        WriteLine("Our value: {0}",sum);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
