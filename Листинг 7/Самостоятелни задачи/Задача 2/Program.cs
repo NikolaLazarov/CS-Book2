a46b004e411032a378b42969a6d25aa22881a316
Nikola Lazarov <hazegamingbg@gmail.com>
2022-07-26 23:27:02 +0300

    Listing 7: :earth_americas:

---
+++ Листинг 7/Самостоятелни задачи/Задача 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+class Program
+{
+    static void meth<T>(T[] arr)
+    {
+        foreach (var item in arr)
+        {
+            Write("|" + $"{item}");
+        }
+        WriteLine("|");
+        arr[0] = arr[arr.Length - 1];
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        foreach (var item in arr)
+        {
+            Write("|" + $"{item}");
+        }
+        WriteLine("|");
+    }
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int[] rndArray = new int[5];
+        for (int i = 0; i < rndArray.Length; i++)
+        {
+            rndArray[i] = rnd.Next(10);
+        }
+        meth(rndArray);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
